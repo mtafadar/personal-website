a1ba26f7869fc90dcf49ac4ea23a21ffe9e49921
Mosrour Tafadar <mosrourtafadar@gmail.com>
2023-06-24 13:10:43 -0500

    adding the blogging page

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ window.addEventListener('load', function() {
 
   for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
+      // Check if the button clicked is the "Blogging" button
+      if (this.innerText.trim() === "Blogging") {
+        // Redirect to a different HTML page
+        window.location.href = "blogging/blog-main.html";
+        return; // Stop further execution of the function
+      }
+
+      // Toggle the active class
       this.classList.toggle("active");
+
       var content = this.nextElementSibling;
       if (content.style.display === "block") {
         content.style.display = "none";
@@ -14,4 +23,3 @@ window.addEventListener('load', function() {
     });
   }
 });
-
